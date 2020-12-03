873b7d984461dd19c7c08f3e11a18ad91c2bc32c
Stephen Herd <stephen_h369@live.co.uk>
2020-12-03 19:08:05 +0000

    day_2

---
+++ day_2/day_2.py
@@ -0,0 +1,39 @@
+arr = []
+
+f = open("day_2_data.txt", "r")
+
+for line in f:
+  arr.append(line.rstrip())
+
+
+def is_valid(line):
+  params, letter, pw = line.split()
+  letter = letter.replace(":", "")
+  minimum, maximum = params.split("-")
+
+  return int(minimum) <= pw.count(letter) <= int(maximum)
+
+def valid_count(arr):
+  count = 0
+  for line in arr:
+    if(is_valid(line)):
+      count+=1
+  return print(count)
+
+def is_valid_2(line):
+  params, letter, pw = line.split()
+  letter = letter.replace(":", "")
+  pos_1, pos_2 = params.split("-")
+  pos_1 = int(pos_1)
+  pos_2 = int(pos_2)
+  return (pw[int(pos_1)-1] == letter) ^ (pw[int(pos_2)-1] == letter)
+
+def valid_count_2(arr):
+  count = 0
+  for line in arr:
+    if(is_valid_2(line)):
+      count+=1
+  return print(count)
+
+valid_count(arr)
+valid_count_2(arr)
